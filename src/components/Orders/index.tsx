bd461ed64823308c75e54e43d9e9bb8b17e56a9b
Miguel Delgado <delgdevoleper@gmail.com>
2025-05-04 16:28:52 -0300

    feat: tipando propriedades recebidas e testando com order mock

--- src/components/Orders/index.tsx
+++ src/components/Orders/index.tsx
@@ -1,12 +1,41 @@
+import { Order } from "../../types/order.ts";
 import { OrdersBoard } from "../OrdersBoard/index.tsx"
 import { Container } from "./styles"
+
+const orders: Order[] = [
+  {
+    _id: '6372e48cbcd195b0d3d0f7f3',
+    table: '3',
+    status: 'WAITING',
+    products: [
+      {
+        product: {
+          name: 'Pizza quatro queijos',
+          imagePath: '1668472896991-quatro-queijos.png',
+          price: 40,
+        },
+        quantity: 3,
+        _id: '6372e48cbcd195b0d3d0f7f4'
+      },
+      {
+        product: {
+          name: 'Coca cola',
+          imagePath: '1668473462705-coca-cola.png',
+          price: 7,
+        },
+        quantity: 2,
+        _id: '6372e48cbcd195b0d3d0f7f5'
+      }
+    ],
+  }
+];
 
 export function Orders() {
   return (
     <Container>
-      <OrdersBoard icon="🕑" title="Fila de espera" quantity="(1)" />
-      <OrdersBoard icon="👩‍🍳" title="Em produção" quantity="(1)" />
-      <OrdersBoard icon="✅" title="Finalizado" quantity="(1)" />
+      <OrdersBoard icon="🕑" title="Fila de espera" orders={orders} />
+      <OrdersBoard icon="👩‍🍳" title="Em produção" orders={[]} />
+      <OrdersBoard icon="✅" title="Finalizado" orders={[]} />
     </Container>
   )
 }
